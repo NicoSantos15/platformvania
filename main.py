4bb3b9104d12a1bfd34363143ebd2f2cf8e2fda8
Nico Santos <sannd001@mymail.unisa.edu.au>
2020-12-08 15:43:22 +1030

    Update main.py
    Change the grid lines and show grid lines. Fixed the default view of the map

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from pygame.locals import *
 
 pygame.init()
 screen_width = 1000
-screen_height = 1000
+screen_height = 600
 
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Platformer')
 
 # Size of tile in the map
-tile_size = 100
+tile_size = 50
 
 # Load image moon
 moon_img = pygame.image.load('Graphics/pixel_moon.jpg')
@@ -21,6 +21,11 @@ moon_img = pygame.transform.scale(moon_img, (200,200))
 # Load image background
 background_img = pygame.image.load('Graphics/dark_background.png')
 background_img = pygame.transform.scale(background_img, (1000,1000))
+
+def show_grid():
+    for line in range(0, 20):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size,0), (line * tile_size, screen_height))
 
 class World():
     def __init__(self, data):
@@ -48,13 +53,20 @@ class World():
             screen.blit(tile[0], tile[1])
 
 
-
+# This is the map, indicates the number of the chosen image to show based on numbers.
 world_data = [
-    [1,1,1,1,1],
-    [1,0,0,0,1],
-    [1,0,0,0,1],
-    [1,0,0,0,1],
-    [1,1,1,1,1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
+[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1],
+[1, 7, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1],
+[1, 0, 2, 0, 0, 7, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
 world = World(world_data)
@@ -69,6 +81,10 @@ while run:
     screen.blit(moon_img, (75,75))
 
     world.draw()
+
+    # Uncomment this to show the grid
+    #show_grid()
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
